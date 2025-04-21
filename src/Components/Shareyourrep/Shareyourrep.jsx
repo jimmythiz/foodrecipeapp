e24a64a5f51ad0a32c33e59c910455d5b965045f
Folajimi <jimmylesky6@gmail.com>
2025-04-21 12:34:54 -0700

    Changed hero picture

--- src/Components/Shareyourrep/Shareyourrep.jsx
+++ src/Components/Shareyourrep/Shareyourrep.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
-import herofood from '../../assets/images/hero food.jpg' 
 import './shareyourrep.css'
 
+const herofood = "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
 const Shareyourrep = () => {
     return ( 
         <div className='shareyourrep-div'>
